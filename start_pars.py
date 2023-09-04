7d7d1325e379ba28031e9d792a03317a3bf5a40a
Sapfire65 <cot65@yandex.ru>
2023-09-04 23:40:06 +0300

    Добавил вариант загрузки для линукс

--- start_pars.py
+++ start_pars.py
@@ -24,13 +24,17 @@ def my_browser_chrome(load_strategy ='normal'):
     # servise = Service(executable_path=ChromeDriverManager(driver_version='114.0.5735.90').install())
 
     # servise = Service(executable_path=ChromeDriverManager().install())
-    servise = Service(executable_path=ChromeDriverManager(driver_version='116.0.5845.96').install())
+    servise = Service(executable_path=ChromeDriverManager().install())
 
-    servise = Service(executable_path="/usr/bin/chromedriver")
+    """Вариант загрузки драйвера для linux"""
+    if not servise:
+        servise = Service(executable_path="/usr/bin/chromedriver")
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = "/usr/bin/chromium-browser"
 
     # Опции запуска Chrome webdriver
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
+
     # chrome_options.page_load_strategy = 'normal'
     chrome_options.page_load_strategy = load_strategy
 
